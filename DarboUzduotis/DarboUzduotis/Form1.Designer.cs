48f31fd06d3a1c727579364211291f86b02ff75a
zzdovydas <60687269+zzdovydas@users.noreply.github.com>
2020-07-02 13:39:26 +0300

    Add files via upload

---
+++ DarboUzduotis/DarboUzduotis/Form1.Designer.cs
@@ -0,0 +1,114 @@
+﻿namespace DarboUzduotis
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listView = new System.Windows.Forms.ListView();
+            this.startButton = new System.Windows.Forms.Button();
+            this.stopButton = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // listView
+            // 
+            this.listView.HideSelection = false;
+            this.listView.Location = new System.Drawing.Point(12, 57);
+            this.listView.Name = "listView";
+            this.listView.Size = new System.Drawing.Size(324, 421);
+            this.listView.TabIndex = 0;
+            this.listView.UseCompatibleStateImageBehavior = false;
+            // 
+            // startButton
+            // 
+            this.startButton.Enabled = false;
+            this.startButton.Location = new System.Drawing.Point(160, 12);
+            this.startButton.Name = "startButton";
+            this.startButton.Size = new System.Drawing.Size(89, 29);
+            this.startButton.TabIndex = 1;
+            this.startButton.Text = "Start";
+            this.startButton.UseVisualStyleBackColor = true;
+            this.startButton.Click += new System.EventHandler(this.startButton_Click);
+            // 
+            // stopButton
+            // 
+            this.stopButton.Enabled = false;
+            this.stopButton.Location = new System.Drawing.Point(255, 12);
+            this.stopButton.Name = "stopButton";
+            this.stopButton.Size = new System.Drawing.Size(81, 29);
+            this.stopButton.TabIndex = 3;
+            this.stopButton.Text = "Stop";
+            this.stopButton.UseVisualStyleBackColor = true;
+            this.stopButton.Click += new System.EventHandler(this.stopButton_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(97, 20);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(49, 20);
+            this.textBox1.TabIndex = 4;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 24);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(79, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Thread number";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(351, 490);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.stopButton);
+            this.Controls.Add(this.startButton);
+            this.Controls.Add(this.listView);
+            this.Name = "Form1";
+            this.Text = "Test";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListView listView;
+        private System.Windows.Forms.Button startButton;
+        private System.Windows.Forms.Button stopButton;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ DarboUzduotis/DarboUzduotis/Form1.cs
@@ -0,0 +1,241 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+using System.Data.OleDb;
+
+namespace DarboUzduotis
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        class ColumnsData
+        {
+            public string columnName;
+            public int columnWidth;
+
+            public ColumnsData(string name, int width)
+            {
+                columnName = name;
+                columnWidth = width;
+            }
+        }
+
+        class RowData
+        {
+            public int rowThreadId;
+            public string rowData;
+
+            public RowData(int threadId, string data)
+            {
+                rowThreadId = threadId;
+                rowData = data;
+            }
+
+        }
+
+        List<Thread> threadList;
+
+        bool threadActive = true;
+
+        List<RowData> rowList;
+
+        public const int rowLimit = 20;
+        OleDbConnection conn = new OleDbConnection();
+
+        private List<ColumnsData> CreateColumns()
+        {
+            List<ColumnsData> cDList = new List<ColumnsData>();
+
+            ColumnsData cD = new ColumnsData("Thread ID", 100);
+            cDList.Add(cD);
+
+            cD = new ColumnsData("Generated data", 200);
+            cDList.Add(cD);
+
+            return cDList;
+        }
+
+        private void DatabaseConnect()
+        {
+            conn.ConnectionString = @"Provider = Microsoft.JET.OLEDB.4.0;Data Source=db.mdb;Persist Security Info=False;";
+            conn.Open();
+        }
+
+        private void DatabaseInsertRow(int threadID, string data)
+        {
+                var date = DateTime.Now;
+                string query = @"INSERT INTO Lentelė1 (ThreadID, TimeGenerated, GeneratedData) VALUES (?, ?, ?);";
+                OleDbCommand command = new OleDbCommand();
+                command.Connection = conn;
+                command.CommandText = query;
+                command.Parameters.AddWithValue("@ThreadID", threadID);
+                command.Parameters.AddWithValue("@TimeGenerated", string.Format("{0}:{1}:{2}", date.Hour, date.Minute, date.Second));
+                command.Parameters.AddWithValue("@GeneratedData", data);
+                command.ExecuteNonQuery();
+        }
+
+        private void InitializeListView()
+        {
+            listView.View = View.Details;
+
+            List<ColumnsData> columnList = CreateColumns();
+
+            for (int i = 0; i < columnList.Count; i++)
+                listView.Columns.Add(columnList[i].columnName, columnList[i].columnWidth);
+
+        }
+
+        private void AddListViewItem(ListViewItem item) => listView.Items.Add(item);
+
+        private void InsertItem(int limit, int threadId, string data)
+        {
+            RowData rD = new RowData(threadId, data);
+
+            rowList.Add(rD);
+
+            int elimination = rowList.Count - limit;
+
+            if (elimination > 0)
+            {
+                for (int i = 0; i < elimination; i++)
+                    rowList.RemoveAt(i);
+            }
+
+        }
+
+        private void UpdateListView()
+        {
+
+            listView.Items.Clear();
+
+            for (int i = 0; i < rowList.Count; i++)
+            {
+                string[] row = new string[] { rowList[i].rowThreadId.ToString(), rowList[i].rowData };
+                ListViewItem item = new ListViewItem(row);
+                listView.Items.Add(item);
+            }
+        }
+
+        private void GenerateThreads(int threadSize, List<Thread> threadList)
+        {
+
+            threadList = new List<Thread>();
+
+            for (int i = 0; i < threadSize; i++)
+            {
+
+                Thread thrd = new Thread(new ThreadStart(() =>
+                {
+                    int threadID = i;
+
+                    Random delayRnd = new Random();
+
+                    int delay = delayRnd.Next(500, 2000);
+
+                    Random lineLengthRnd = new Random();
+                    Random symbolRnd = new Random();
+
+                    while (threadActive)
+                    {
+
+                        Thread.Sleep(delay);
+
+                        int lineLength = lineLengthRnd.Next(5, 10);
+                        string line = "";
+
+                        for (int j = 0; j < lineLength; j++)
+                        {
+                            int symbolIndex = symbolRnd.Next(41, 126);
+
+                            line += (char)symbolIndex;
+                        }
+
+                        if (threadActive)
+                        {
+                            Invoke(new Action(() =>
+                            {
+                                InsertItem(rowLimit, threadID, line);
+                                UpdateListView();
+                                DatabaseInsertRow(threadID, line);
+                            }));
+                        }
+                    }
+
+                }));
+
+                threadList.Add(thrd);
+                Thread.Sleep(50);
+                threadList[i].Start();
+            }
+
+        }
+
+        private void TerminateThreads()
+        {
+
+            threadActive = false;
+
+            Thread.Sleep(50);
+
+            for (int i = 0; i < threadList.Count; i++)
+                threadList[i].Abort();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            threadList = new List<Thread>();
+            rowList = new List<RowData>();
+
+            InitializeListView();
+            DatabaseConnect();
+        }
+
+        private void startButton_Click(object sender, EventArgs e)
+        {
+            int threadNumber = int.Parse(textBox1.Text);
+            GenerateThreads(threadNumber, threadList);
+            stopButton.Enabled = true;
+            startButton.Enabled = false;
+        }
+
+        private void stopButton_Click(object sender, EventArgs e)
+        {
+            TerminateThreads();
+
+            conn.Close();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            try
+            {
+                int number = int.Parse(textBox1.Text);
+
+                if (number >= 2 && number <= 15)
+                    startButton.Enabled = true;
+                else
+                    startButton.Enabled = false;
+            }
+            catch
+            {
+                startButton.Enabled = false;
+            }
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            TerminateThreads();
+        }
+    }
+}
